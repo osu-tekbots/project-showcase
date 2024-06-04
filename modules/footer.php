be86418d023ca71cd30be4b2246a9b8b4174837a
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2024-06-04 16:47:05 -0700

    Add button to feedback form

--- modules/footer.php
+++ modules/footer.php
@@ -2,6 +2,11 @@
 <?php 
 if(!isset($contentOnly) || !$contentOnly): 
 ?>
+<!-- ?entry.1671399415=Project Showcase specifies checkbox to select for question 1 -->
+<a href="https://docs.google.com/forms/d/e/1FAIpQLSdK6-dYdAUel_5yGWeJWiO7ptoXFscGZzRHhRI4FY7I1BDRog/viewform?entry.1671399415=Project Showcase" target="_blank" class="btn" type="button" style="position: fixed; right: 20px; bottom: 20px; z-index: 1001; background: rgb(195, 69, 0); color: #fff; font-size: 1rem; padding: .375rem .75rem; border-radius: 0.25rem; font-weight: normal;">
+    <i class="far fa-comment mr-1"></i>
+    Feedback
+</a>
 <footer>
     &copy;&nbsp;<?php echo date('Y'); ?> Oregon State University
     <a class="disclaimer" href="https://oregonstate.edu/official-web-disclaimer" target="_blank">Disclaimer</a>
