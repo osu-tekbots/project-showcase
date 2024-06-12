a4dc321d7cd217b338324146e7f2ea6f0a25171e
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2024-06-12 15:43:35 -0700

    Add attribution for Google Form query string

--- modules/footer.php
+++ modules/footer.php
@@ -3,6 +3,7 @@
 if(!isset($contentOnly) || !$contentOnly): 
 ?>
 <!-- ?entry.1671399415=Project Showcase specifies checkbox to select for question 1 -->
+<!-- See: https://theconfuzedsourcecode.wordpress.com/2019/11/10/lets-auto-fill-google-forms-with-url-parameters/ -->
 <a href="https://docs.google.com/forms/d/e/1FAIpQLSdK6-dYdAUel_5yGWeJWiO7ptoXFscGZzRHhRI4FY7I1BDRog/viewform?entry.1671399415=Project Showcase" target="_blank" class="btn" type="button" style="position: fixed; right: 20px; bottom: 20px; z-index: 1001; background: rgb(195, 69, 0); color: #fff; font-size: 1rem; padding: .375rem .75rem; border-radius: 0.25rem; font-weight: normal;">
     <i class="far fa-comment mr-1"></i>
     Feedback
